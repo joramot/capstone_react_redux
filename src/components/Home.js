4aa4429467329660ae1d924de1167a38693d8bea
JOSE RAUL MOJICA <mojica.raul@gmail.com>
2023-10-17 15:17:11 -0600

    Define imports in home.js to get useEffect, Link, useSelector, useDispatch and fetchData and getId and implement

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchData, getId } from '../redux/home/homeSlice';
 
-const Home = () => <div> This is the HOME</div>;
+const Home = () => {
+  console.log(fetchData.data);
+  const {
+    isPending, error, data,
+  } = useSelector((state) => state.home);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
+  const handleGetId = (id) => {
+    dispatch(getId(id));
+  };
+
+  return (
+    <>This the HOME</>
+  );
+};
 
 export default Home;
