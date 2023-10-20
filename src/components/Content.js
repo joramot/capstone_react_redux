1b5781bcbb5717dcb6fc2fbecb8d592010b86cb1
Jose RM <mojica.raul@gmail.com>
2023-10-20 11:31:06 -0600

    Add the styles in the Content.js component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import X from '../images/x-mark.png';
+import '../App.css';
 
 const Content = () => {
   const { data, id } = useSelector((store) => store.home);
 
   return (
     <div>
-      <div className="">
-        <Link to="/" className="">
+      <div className="cross-container">
+        <Link to="/" className="cross-icon">
           <img src={X} alt="cross" />
         </Link>
       </div>
-      <div className="">
+      <div className="details-container">
         <h1>{data[id].symbol}</h1>
         <h2>{data[id].name}</h2>
         <h3>
